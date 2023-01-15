76c0212042b448b0c7f9cf3ac537e233d7f48d4d
shivam jain <113499149+shivamj2020@users.noreply.github.com>
2023-01-15 16:01:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,20 @@
-import React, { useState } from "react";
-import { Todo } from "./Todo";
-import "../styles/App.css";
-
-const AddTodo = ({ dispatch, state }) => {
-  const [text, setText] = useState("");
-
-  function handleOnSubmit(e) {
-    e.preventDefault();
-    dispatch({ type: "ADD", value: text });
-    setText("");
-  }
-
-  function handleDelete(id) {
-    const newData = state.todos.filter((e, i) => e.id !== id);
-    dispatch({ type: "DELETE", data: newData });
-  }
-
+import React, { useReducer } from 'react'
+import { todoReducer } from '../reducers/todo';
+import '../styles/App.css';
+import { AddTodo } from './AddTodo';
+import { Todo } from './Todo';
+const App = () => {
+  const [state,dispatch] = useReducer(todoReducer,[]);
+  
   return (
-    <>
-      <form id="todo-form" onSubmit={(e) => handleOnSubmit(e)}>
-        <input
-          type="text"
-          id="todo-input"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button className="addBtn">Add</button>
-      </form>
-      <div className="todoList">
-        {state.todos.map((e, i) => (
-          <Todo key={e.id} {...e} handleDelete={handleDelete} />
-        ))}
-      </div>
-    </>
-  );
-};
+    <div id="main">
+      <AddTodo dispatch={dispatch}/>
+     {state.map(todo => {
+      return <Todo title={todo.title} key={todo.id} dispatch = {dispatch} id={todo.id}/>
+     })}
+    </div>
+  )
+}
 
-export { AddTodo };
+
+export default App;
