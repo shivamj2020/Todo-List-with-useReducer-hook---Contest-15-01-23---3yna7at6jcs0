27238f5ed2de7a20e55e38b44c3fd9bac82d4e01
shivam jain <113499149+shivamj2020@users.noreply.github.com>
2023-01-15 15:40:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
-import React, { useReducer } from 'react'
-import { todoReducer } from '../reducers/todo';
-import '../styles/App.css';
-import { AddTodo } from './AddTodo';
-import { Todo } from './Todo';
-const App = () => {
-  const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
-  
- 
+import React, { useState } from "react";
+import { Todo } from "./Todo";
+import "../styles/App.css";
+
+const AddTodo = ({ dispatch, state }) => {
+  const [text, setText] = useState("");
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: "ADD", value: text });
+    setText("");
+  }
+
+  function handleDelete(id) {
+    const newData = state.todos.filter((e, i) => e.id !== id);
+    dispatch({ type: "DELETE", data: newData });
+  }
+
   return (
-    <div id="main">
-      <AddTodo />
-     {/* Render list of Todo Components here */}
-    </div>
-  )
-}
+    <>
+      <form id="todo-form" onSubmit={(e) => handleOnSubmit(e)}>
+        <input
+          type="text"
+          id="todo-input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="addBtn">Add</button>
+      </form>
+      <div className="todoList">
+        {state.todos.map((e, i) => (
+          <Todo key={e.id} {...e} handleDelete={handleDelete} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-
-export default App;
+export { AddTodo };
